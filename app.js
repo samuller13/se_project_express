875631934ddb35ee9810d4c0793abe7d1ee44be8
Samantha Muller <samuller13@gmail.com>
2026-01-19 13:29:02 -0500

    remove unused variable in app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
 const { createUser, login } = require("./controllers/users");
-const auth = require("./middlewares/auth");
 
 const app = express();
 const { PORT = 3001 } = process.env;
