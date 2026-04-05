ffdb5aa0eb7183e54cbbf8078b375584b0dee537
Samantha Muller <samuller13@gmail.com>
2026-04-05 11:34:01 -0400

    express project commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db").then(() => {
-  // console.log("Connected to DB");
+  console.log("Connected to DB");
 });
 // .catch(console.error);
 
